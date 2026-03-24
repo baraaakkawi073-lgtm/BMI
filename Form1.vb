c1e8fd9e58022fde873f4ce39076fc470965c9e8
baraaakkawi073-lgtm <baraaakkawi073@gmail.com>
2026-03-24 21:44:10 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text = "" Then Or TextBox2.Text = "" Then
+            MsgBox("enter the number")
+        Else
+        TextBox3.Text = TextBox1.Text / TextBox2.Text ^ 2
+        End
+
+
+    End Sub
